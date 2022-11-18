e0537d8ac35b23695b53569cb043964814593f81
begimaiz <117903250+begimaiz@users.noreply.github.com>
2022-11-18 19:08:55 +0600

    new game method added

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ def generate_turn():
         letter = 'n'
     return letter
 
+
+def is_wrong(letter):
+    if len(letter) == 1:
+        if letter in 'rsp':
+            return False
+        else:
+            return True
+    else:
+        return True
+
+def new_game():
+
+    game_limit = 5
+    wrong_input = True
+
+    while wrong_input and game_limit > 0:
+        user_input = input('enter one of there three to make a move R S P: ')
+        user_input = user_input.lower()
+        wrong_input = is_wrong(user_input)
+        game_limit -= 1
+
+new_game()
